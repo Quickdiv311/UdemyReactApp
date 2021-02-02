22fa8a9fb02b8564aef90a5969ae07cd14e8c675
Quickdiv311 <dagarwal058@gmail.com>
2021-02-02 14:17:56 +0530

    homepage added

---
+++ src/pages/homepage/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Directory from '../../components/directory/directory';
+import './home.scss';
+
+const Homepage = () => (
+    <div className="home-page">
+        <Directory/>
+    </div>
+)
+
+export default Homepage;
